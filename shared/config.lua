25157a78a7d4744062ffdb1b6187300c7fdf3964
Amanda Roche <142677539+stitchpetri@users.noreply.github.com>
2025-09-11 01:43:56 +0000

    fixed coords

--- shared/config.lua
+++ shared/config.lua
@@ -10,6 +10,6 @@ Config.TestCourse = {
   mode  = "time_trial",
   radius = 5.0,
 
-  start  = vector3(-280.0, 804.0, 119.0),
-  finish = vector3(-260.0, 788.0, 117.0),
+  start  = vector3(26.12, -109.78, 104.70),
+  finish = vector3(32.12, -93.78, 104.70),
 }
